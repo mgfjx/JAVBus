775b9191c686de7a7567b0125a1c0bdc1999e9bf
mgfjx <mgfjxxiexiaolong@gmail.com>
2020-03-14 01:24:20 +0800

    fix

--- JAVBus/Common/GGDriveFileCell.m
+++ JAVBus/Common/GGDriveFileCell.m
@@ -12,6 +12,8 @@
 
 @property (weak, nonatomic) IBOutlet UILabel *titleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *recoverBtn;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+@property (weak, nonatomic) IBOutlet UILabel *sizeLabel;
 
 @end
 
@@ -21,6 +23,13 @@
     _file = file;
     
     _titleLabel.text = file.name;
+    NSDate *date = file.clientModified;
+    NSDateFormatter *formate = [[NSDateFormatter alloc] init];
+    formate.dateFormat = @"yyyy-MM-dd HH:mm:ss";
+    self.dateLabel.text = [formate stringFromDate:date];
+    NSInteger size = [file.size integerValue];
+    NSString *text = [NSString stringWithFormat:@"%.1fm", size*1.0/(1024*1024)];
+    self.sizeLabel.text = text;
     if ([file.name isEqualToString:@"JavBus.db"]) {
         self.recoverBtn.hidden = NO;
     }else{
